1006a021f475f157caf964c5a31c6c8e32603166
Neos-p <71880149+Neos-p@users.noreply.github.com>
2023-03-28 11:33:10 -0400

    Added UI
    Title menu
    Button Select

---
+++ Assets/UiManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public Button[] buttons;
+    //Make sure button 0 is start button
+    private void Start()
+    {
+        buttons[0].Select();
+    }
+    
+    public void ChangeScene(string SceneName)
+    {
+        //Play animation?
+        Debug.Log("AAAAAA");
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
